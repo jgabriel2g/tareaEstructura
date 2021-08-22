b539a9ae5adc925bb5f1999663e87b1bd6608891
Nelson Morales <camilomorales2615@gmail.com>
2021-08-22 17:24:16 -0500

    Clean Code

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
 <?php 
-echo "kabkusydkdcbk";
-echo "nelson"
-echo "nelson"
-echo "nelson"
-echo "nelson"
-echo "nelson"
-echo "nelson"
-echo "nelson"
 
-echo "yan";
-echo "yan";
-echo "yan";
-echo "yan";
-echo "yan";
-echo "yan";
-echo "yan";
-echo "yan";
-
-echo "Jesus";
-echo "Jesus";
-echo "Jesus";
-echo "Jesus";
-echo "Jesus";
-echo "Jesus";
-echo "Jesus";
-echo "Jesus";
-echo "Hola";
 ?>
